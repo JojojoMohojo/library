095a406c1295034637e98fe1dbf91ff5184ea713
Joe Pattenden <151946049+JojojoMohojo@users.noreply.github.com>
2025-01-15 20:04:22 +0000

    add display and close book functionality

--- script.js
+++ script.js
@@ -39,29 +39,54 @@ function addBookToLibrary(title, author, genres, pages, haveRead, published, seq
 }
 
 const shelf = document.querySelector(".shelf");
+const display = document.querySelector(".book-display");
+const backdrop = document.querySelector(".modal-backdrop");
+const closeButton = document.querySelector("#close-book");
+display.style.display = "none";
 
-function displayBook() {
+closeButton.addEventListener("click", () => {
+    closeBook();
+})
+
+function addBooksToShelf() {
     while (shelf.querySelector('.book')) {
         shelf.querySelector('.book').remove();
     }
     myLibrary.forEach( (item) => {
-        const book = document.createElement("div");
-        book.id = `Book-${item.id}`;
-        console.log(`${item.id}`);
-        book.classList.add("book-container", "book");
-        book.textContent = item.title;
-        const bookIcon = document.createElement("div");
-        bookIcon.classList.add("icon", "open-book-icon");
-        bookIcon.addEventListener("click", () => {
-            displayBookContent(item);
-        });
-        book.appendChild(bookIcon);
-        shelf.appendChild(book);
+        createBook(item);
     })
 }
 
-function displayBookContent(book) {
-    console.log(`Clicked on ${book.title}`);
+function createBook(item) {
+    const book = document.createElement("div");
+    book.id = `Book-${item.id}`;
+    book.classList.add("book-container", "book");
+    book.textContent = item.title;
+    const bookIcon = document.createElement("div");
+    bookIcon.classList.add("icon", "open-book-icon");
+    bookIcon.addEventListener("click", () => {
+        displayBook(item);
+    });
+    book.appendChild(bookIcon);
+    shelf.appendChild(book);
 }
 
-displayBook();
+function displayBook(book) {
+    console.log(`Clicked on ${book.title}`);
+    document.getElementById('book-title').textContent = `Title: ${book.title}`;
+    document.getElementById('book-author').textContent = `Author: ${book.author}`;
+    document.getElementById('book-genres').textContent = `Genres: ${book.genres}`;
+    document.getElementById('book-pages').textContent = `Pages: ${book.pages}`;
+    document.getElementById('book-published').textContent = `Published: ${book.published}`;
+    document.getElementById('book-read').textContent = `Read: ${book.haveRead}`;
+    document.getElementById('book-sequels').textContent = `Sequels: ${book.sequels}`;
+    display.style.display = "flex";
+    backdrop.style.display = "block";
+}
+
+function closeBook() {
+    display.style.display = "none";
+    backdrop.style.display = "none";
+}
+
+addBooksToShelf();
