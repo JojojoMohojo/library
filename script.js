13761384e6f87df2628ebcdf8d8a95c47bf5bbda
Joe Pattenden <151946049+JojojoMohojo@users.noreply.github.com>
2025-01-26 15:44:18 +0000

    complete cosmetic css

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ const haveReadInput = document.querySelector("#have-read-input");
 const closeFormButton = document.querySelector("#close-form");
 const submitButton = document.querySelector("#submit-button");
 
+const openBookIconSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M560-564v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-600q-38 0-73 9.5T560-564Zm0 220v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-380q-38 0-73 9t-67 27Zm0-110v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-490q-38 0-73 9.5T560-454ZM260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z"/></svg>
+`;
+
+const removeBookIconSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+`;
+
+const addBookIconSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>
+`;
+
 let currentBook;
 
 function Book(title, author, genres, pages, published, sequels, haveRead) {
@@ -63,7 +75,8 @@ changeReadButton.addEventListener("click", () => {
 
 closeFormButton.addEventListener("click", () => {
     event.preventDefault();
-    clearForm()
+    clearForm();
+    clearErrorMessages();
     newBookForm.close();
 })
 
@@ -110,18 +123,24 @@ function createBook(item) {
     book.id = `Book-${item.id}`;
     book.classList.add("book-container", "book");
     book.textContent = item.title;
+
     const iconContainer = document.createElement("div");
     iconContainer.classList.add("icon-container");
+
     const openIcon = document.createElement("div");
     openIcon.classList.add("icon", "open-book-icon");
+    openIcon.innerHTML = openBookIconSVG;
     openIcon.addEventListener("click", () => {
         displayBook(item);
     });
+
     const removeIcon = document.createElement("div");
     removeIcon.classList.add("icon", "remove-book-icon");
+    removeIcon.innerHTML = removeBookIconSVG;
     removeIcon.addEventListener("click", () => {
         removeBook(item);
     });
+
     book.appendChild(iconContainer);
     iconContainer.appendChild(openIcon);
     iconContainer.appendChild(removeIcon);
